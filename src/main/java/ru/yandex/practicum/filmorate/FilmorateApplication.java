abd4cffa38f2d1d2fb1f5f459a0f6bd96d788424
FORES <master-fores@yandex.ru>
2024-05-18 12:18:31 +0300

    Коррекция ошибок

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.validation.annotation.Validated;
 
 @SpringBootApplication
 @Slf4j
