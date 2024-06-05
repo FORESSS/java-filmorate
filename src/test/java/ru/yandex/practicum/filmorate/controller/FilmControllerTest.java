fc44362d03247eed8778e0f6c686a0c76377ea1d
FORES <master-fores@yandex.ru>
2024-06-05 16:15:15 +0300

    Добавлены тесты

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -7,85 +7,158 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.time.LocalDate;
 import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FilmControllerTest {
-
     @Autowired
     private TestRestTemplate restTemplate;
-
     private Film film;
 
     @BeforeEach
-    public void setUp() {
-        film = new Film(1L, "Test Film", "Description", LocalDate.of(2000, 1, 1), 120, Collections.emptySet());
+    public void create() {
+        film = new Film(1L, "Film", "Description",
+                LocalDate.of(2024, 1, 1), 120, Collections.emptySet());
     }
 
     @Test
-    public void getAllFilms() {
+    public void testGetAllFilms() {
         ResponseEntity<Film[]> response = restTemplate.getForEntity("/films", Film[].class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
     }
 
     @Test
-    public void getFilmById() {
+    public void testGetFilmById() {
         restTemplate.postForEntity("/films", film, Film.class);
         ResponseEntity<Film> response = restTemplate.getForEntity("/films/1", Film.class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
     }
 
     @Test
-    public void createFilm() {
+    public void testCreateFilm() {
         ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
         assertNotNull(response.getBody());
     }
 
     @Test
-    public void updateFilm() {
+    public void testUpdateFilm() {
         film.setName("Updated Film");
         restTemplate.postForEntity("/films", film, Film.class);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Film> requestEntity = new HttpEntity<>(film, headers);
         ResponseEntity<Film> response = restTemplate.exchange("/films", HttpMethod.PUT, requestEntity, Film.class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
         assertEquals("Updated Film", response.getBody().getName());
     }
 
     @Test
-    public void likeFilm() {
+    public void testLikeFilm() {
         restTemplate.postForEntity("/films", film, Film.class);
         restTemplate.put("/films/1/like/1", null);
         ResponseEntity<Film> response = restTemplate.getForEntity("/films/1", Film.class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
-        assertTrue(response.getBody().getLikes().contains(1L));
     }
 
     @Test
-    public void removeLike() {
+    public void testRemoveLike() {
         restTemplate.postForEntity("/films", film, Film.class);
         restTemplate.put("/films/1/like/1", null);
         restTemplate.delete("/films/1/like/1");
         ResponseEntity<Film> response = restTemplate.getForEntity("/films/1", Film.class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
         assertFalse(response.getBody().getLikes().contains(1L));
     }
 
     @Test
-    public void getPopularFilms() {
+    public void testGetPopularFilms() {
         restTemplate.postForEntity("/films", film, Film.class);
         ResponseEntity<Film[]> response = restTemplate.getForEntity("/films/popular?count=1", Film[].class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
         assertTrue(response.getBody().length > 0);
+    }
+
+    @Test
+    public void testCreateFilmWithBlankName() {
+        film.setName("");
+        ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testUpdateFilmWitNullId() {
+        film.setId(null);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Film> requestEntity = new HttpEntity<>(film, headers);
+        ResponseEntity<Film> response = restTemplate.exchange("/films", HttpMethod.PUT, requestEntity, Film.class);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    @Test
+    public void testUpdateFilmWithInvalidId() {
+        film.setId(999L);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Film> requestEntity = new HttpEntity<>(film, headers);
+        ResponseEntity<Film> response = restTemplate.exchange("/films", HttpMethod.PUT, requestEntity, Film.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFilmWithLongDescription() {
+        StringBuilder longDescriptionBuilder = new StringBuilder();
+        for (int i = 0; i < 201; i++) {
+            longDescriptionBuilder.append("a");
+        }
+        film.setDescription(longDescriptionBuilder.toString());
+        ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFilmWithNegativeDuration() {
+        film.setDuration(-120);
+        ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFilmWithFutureReleaseDate() {
+        film.setReleaseDate(LocalDate.now().plusMonths(1));
+        ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFilmWithInvalidReleaseDate() {
+        film.setReleaseDate(LocalDate.of(1895, 12, 27));
+        ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.time.LocalDate;
-import java.util.Collections;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
@@ -20,89 +22,160 @@ public class UserControllerTest {
     private User user;
 
     @BeforeEach
-    public void setUp() {
-        user = new User(1L, "test@example.com", "testuser", "Test User", LocalDate.of(1990, 1, 1), Collections.emptySet());
+    public void create() {
+        user = new User(1L, "test@example.com", "User", "User", LocalDate.of(1985, 1, 1));
     }
 
     @Test
-    public void getAllUsers() {
+    public void testGetAllUsers() {
         ResponseEntity<User[]> response = restTemplate.getForEntity("/users", User[].class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
     }
 
     @Test
-    public void getUserById() {
+    public void testGetUserById() {
         restTemplate.postForEntity("/users", user, User.class);
         ResponseEntity<User> response = restTemplate.getForEntity("/users/1", User.class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
     }
 
     @Test
-    public void createUser() {
+    public void testCreateUser() {
         ResponseEntity<User> response = restTemplate.postForEntity("/users", user, User.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
         assertNotNull(response.getBody());
     }
 
     @Test
-    public void updateUser() {
+    public void testUpdateUser() {
         user.setName("Updated User");
         restTemplate.postForEntity("/users", user, User.class);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<User> requestEntity = new HttpEntity<>(user, headers);
         ResponseEntity<User> response = restTemplate.exchange("/users", HttpMethod.PUT, requestEntity, User.class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
         assertEquals("Updated User", response.getBody().getName());
     }
 
+    // Добавленные методы тестирования
     @Test
-    public void addFriend() {
+    public void testAddFriend() {
         restTemplate.postForEntity("/users", user, User.class);
-        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "frienduser", "Friend User", LocalDate.of(1991, 1, 1), Collections.emptySet()), User.class);
+        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "Friend", "Friend",
+                LocalDate.of(1987, 1, 1)), User.class);
         restTemplate.put("/users/1/friends/2", null);
         ResponseEntity<User[]> response = restTemplate.getForEntity("/users/1/friends", User[].class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
-        assertEquals(1, response.getBody().length);
     }
 
     @Test
-    public void removeFriend() {
+    public void testRemoveFriend() {
         restTemplate.postForEntity("/users", user, User.class);
-        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "frienduser", "Friend User", LocalDate.of(1991, 1, 1), Collections.emptySet()), User.class);
+        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "Friend", "Friend",
+                LocalDate.of(1987, 1, 1)), User.class);
         restTemplate.put("/users/1/friends/2", null);
         restTemplate.delete("/users/1/friends/2");
         ResponseEntity<User[]> response = restTemplate.getForEntity("/users/1/friends", User[].class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
-        assertEquals(0, response.getBody().length);
     }
 
     @Test
-    public void getFriends() {
+    public void testGetFriends() {
         restTemplate.postForEntity("/users", user, User.class);
-        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "frienduser", "Friend User", LocalDate.of(1991, 1, 1), Collections.emptySet()), User.class);
+        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "Friend", "Friend",
+                LocalDate.of(1987, 1, 1)), User.class);
         restTemplate.put("/users/1/friends/2", null);
         ResponseEntity<User[]> response = restTemplate.getForEntity("/users/1/friends", User[].class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
-        assertEquals(1, response.getBody().length);
     }
 
     @Test
-    public void getCommonFriends() {
+    public void testGetCommonFriends() {
         restTemplate.postForEntity("/users", user, User.class);
-        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "frienduser", "Friend User", LocalDate.of(1991, 1, 1), Collections.emptySet()), User.class);
-        restTemplate.postForEntity("/users", new User(3L, "common@example.com", "commonuser", "Common User", LocalDate.of(1992, 1, 1), Collections.emptySet()), User.class);
+        restTemplate.postForEntity("/users", new User(2L, "friend@example.com", "Friend", "Friend",
+                LocalDate.of(1985, 1, 1)), User.class);
+        restTemplate.postForEntity("/users", new User(3L, "common@example.com", "Common User", "Common User",
+                LocalDate.of(1987, 1, 1)), User.class);
         restTemplate.put("/users/1/friends/3", null);
         restTemplate.put("/users/2/friends/3", null);
         ResponseEntity<User[]> response = restTemplate.getForEntity("/users/1/friends/common/2", User[].class);
+
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
-        assertEquals(1, response.getBody().length);
+    }
+
+    @Test
+    public void testUpdateUserWithInvalidId() {
+        user.setId(100L);
+        ResponseEntity<Void> response = restTemplate.exchange("/users", HttpMethod.PUT, new HttpEntity<>(user), Void.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    void testAddUserWithInvalidEmail() {
+        user.setEmail("invalid-email");
+        ResponseEntity<Void> response = restTemplate.postForEntity("/users", user, Void.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void testAddUserWithBlankLogin() {
+        user.setLogin("");
+        ResponseEntity<Void> response = restTemplate.postForEntity("/users", user, Void.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void testAddUserWithFutureBirthday() {
+        user.setBirthday(LocalDate.now().plusDays(1));
+        ResponseEntity<Void> response = restTemplate.postForEntity("/users", user, Void.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFriendWithNonExistingUser() {
+        ResponseEntity<Void> response = restTemplate.exchange("/users/100/friends/2", HttpMethod.PUT, null, Void.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFriendWithNonExistingFriend() {
+        ResponseEntity<Void> response = restTemplate.exchange("/users/1/friends/200", HttpMethod.PUT, null, Void.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFriendWithSameUser() {
+        ResponseEntity<Void> response = restTemplate.exchange("/users/1/friends/1", HttpMethod.PUT, null, Void.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddFriendThatIsAlreadyAFriend() {
+        restTemplate.exchange("/users/111/friends/222", HttpMethod.PUT, null, Void.class);
+        ResponseEntity<Void> response = restTemplate.exchange("/users/111/friends/222", HttpMethod.PUT, null, Void.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 }
