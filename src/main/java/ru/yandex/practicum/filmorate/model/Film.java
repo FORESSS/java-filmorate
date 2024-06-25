8f646755c02a3b25be8be4b79d00a47f56f8a860
Сергей <master-fores@yandex.ru>
2024-06-25 19:49:57 +0300

    Обновлены модели

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,34 +1,19 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@AllArgsConstructor
+@Builder
 public class Film {
-    private Long id;
-
-    @NotBlank(message = "Поле не может быть пустым")
+    private int id;
     private String name;
-
-    @NotNull(message = "Обязательное поле")
-    @Size(max = 200, message = "Должно быть до 200 символов")
     private String description;
-
-    @NotNull(message = "Обязательное поле")
+    private Set<Integer> genresId;
+    private Integer mpaId;
     private LocalDate releaseDate;
-
-    @NotNull(message = "Обязательное поле")
-    @Positive(message = "Продолжительность должна быть положительной")
-    private Integer duration;
-
-    private final Set<Long> likes = new HashSet<>();
+    private int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,27 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
+@Builder
 public class User {
-    private Long id;
-
-    @NotBlank(message = "Поле e-mail не может быть пустым")
-    @Email(message = "Должен быть корректный e-mail")
+    private Integer id;
     private String email;
-
-    @NotBlank(message = "Поле login не может быть пустым")
-    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелов")
     private String login;
-
     private String name;
-
-    @NotNull(message = "Обязательное поле")
-    @Past(message = "Дата рождения должна быть меньше текущей даты")
     private LocalDate birthday;
 }
